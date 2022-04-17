bbb622d9e3ea688a60b5b98c5985c3562b314f11
arkadi_riefer <a.riefer@default-digital.com>
2022-04-17 12:14:12 +0200

    just spring setup with 'spring web', 'spring security', 'spring data jpa', 'mysql driver' from 'start.spring.io'. no changes.

---
+++ src/main/java/de/akadd/springchat/SpringChatApplication.java
@@ -0,0 +1,13 @@
+package de.akadd.springchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/akadd/springchat/SpringChatApplicationTests.java
@@ -0,0 +1,13 @@
+package de.akadd.springchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
